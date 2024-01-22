ffd7e5c14482adb1a9599f3a701370d9dce5fe8a
Jacob Dixon <jacob.dixon@live.com>
2024-01-22 09:54:13 -0400

    introduced routes and models

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//importing required modules (alot are dependencies)
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+//log information of the requestion to the console.
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
 
+//establish connection to the database(mongoDB)
+mongoose.connect("mongodb://localhost:27017/stock_market");
+
+//defining our routers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
+//define the app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//defining our middleware.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
